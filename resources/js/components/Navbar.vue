4cf9c0b38e17e18f9a3c2a554cbeaaeaee7f200d
Lucas Azevedo <lucas.elaio@gmail.com>
2023-08-03 16:34:14 -0400

    ok

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,19 +1,39 @@
 <template>
-  <nav class="w-full navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Home</a>
-      <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/resume">Resume</a>
-        </li>
-      </ul>
-    </div>
-    </div>
-  </nav>
+    <nav class="w-full navbar navbar-expand-lg" data-bs-theme="dark">
+        <div class="container-fluid">
+            <div style="color: white; margin: 5px 35px;">
+                <i class="fa-solid fa-person-chalkboard fa-xl"></i>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/resume">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/">Work</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex" role="search">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="https://www.linkedin.com/in/lucas-v-azevedo/" target="_blank" class="nav-link"><i class="fa-brands fa-linkedin fa-lg"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </template>
+
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
 }
 </script>
+
+<style lang="scss" scoped>
+nav {
+    background-color: black;
+    font-size: 25px;
+}
+</style>
